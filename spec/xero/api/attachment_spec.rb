447170ff1035103ed251bf203cf95450bda0f377
Christian <zarski@gmail.com>
2018-10-25 08:52:02 -0400

    Move attachment VCR location

--- spec/xero/api/attachment_spec.rb
+++ spec/xero/api/attachment_spec.rb
@@ -5,7 +5,7 @@ describe Xero::Api::Attachment do
   let(:api){ Xero::Api.new(creds.to_h) }
 
   it 'for an invoice is successfully created' do
-    use_cassette("xero_api/attachment/create_for_invoice", record: :none) do
+    use_cassette("attachment/create_for_invoice", record: :none) do
       file_name = 'connect_xero_button_blue_2x.png'
       resp = api.upload_attachment(:invoices, id: '9eb7b996-4ac6-4cf8-8ee8-eb30d6e572e3', 
                                    file_name: file_name, content_type: 'image/png',
